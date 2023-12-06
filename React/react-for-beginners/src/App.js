01e49b59ae8b5b35cc9a9b7165f91a73ceb744df
KuMMii <4.prgmng@gmail.com>
2023-12-06 23:32:46 +0900

    6-2 useEffect

--- React/react-for-beginners/src/App.js
+++ React/react-for-beginners/src/App.js
@@ -1,11 +1,29 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+	const [counter, setValue] = useState(0);
+	const [keyword, setKeyword] = useState("");
+	const onClick = () => setValue((prev) => prev + 1);
+	const onChange = (event) => setKeyword(event.target.value);
+	// console.log("i run all the time");
+
+	useEffect(() => {
+		console.log("I run only once");
+	}, []);
+	useEffect(() => {
+		console.log("I run when when 'keyword' changes");
+	}, [keyword]);
+	useEffect(() => {
+		console.log("I run when when 'counter' changes");
+	}, [counter]);
+	useEffect(() => {
+		console.log("I run when when keyword & counter change");
+	}, [keyword, counter]);
 	return (
 		<div>
-			<h1 className={styles.title}>Welcome back to React!!!!</h1>
-			<Button text={"Continue"} />
+			<input value={keyword} onChange={onChange} type="text" placeholder="Search here" />
+			<h1>{counter}</h1>
+			<button onClick={onClick}>click me</button>
 		</div>
 	);
 }
--- React/react-for-beginners/src/index.js
+++ React/react-for-beginners/src/index.js
@@ -4,7 +4,10 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
+	// StrickMode는 create-react-app으로 설치를 하시면 기본적으로 생성이 되어있는 태그인데
+	// 해당 태그로 감싸져 있는 경우에는 코드의 문제를 감지하고 경고하기 위해서
+	// 구성 요소를 두 번 렌더링 한다고 합니다.(개발용이 아닌 프로덕션용)
+	// <React.StrictMode>
+	<App />,
+	// </React.StrictMode>,
 );

d43226dcac1863888e36524bc5b4b2defb85415b
KuMMii <4.prgmng@gmail.com>
2023-12-07 22:24:24 +0900

    7-5 router

--- React/react-for-beginners/src/App.js
+++ React/react-for-beginners/src/App.js
@@ -1,42 +1,15 @@
-import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-	const [loading, setLoading] = useState(true);
-	const [coins, setCoins] = useState([]);
-	const [amount, setAmount] = useState(0);
-	const onChange = (event) => {
-		setAmount(event.target.value);
-	};
-	useEffect(() => {
-		fetch("https://api.coinpaprika.com/v1/tickers?limit=10")
-			.then((response) => response.json())
-			.then((json) => {
-				setCoins(json);
-				setLoading(false);
-			});
-	}, []);
-
 	return (
-		<div>
-			<h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-			{loading ? (
-				<strong>Loading...</strong>
-			) : (
-				<div>
-					<input type="number" placeholder="Type the amount" value={amount} onChange={onChange} />
-					<br />
-					<br />
-					<select>
-						{coins.map((coin) => (
-							<option key={coin.id}>
-								{coin.name} ({coin.symbol}) : $
-								{amount === 0 ? coin.quotes.USD.price : coin.quotes.USD.price / amount} USD
-							</option>
-						))}
-					</select>
-				</div>
-			)}
-		</div>
+		<Router>
+			<Routes>
+				<Route path="/" element={<Home />}></Route>
+				<Route path="/movie" element={<Detail />}></Route>
+			</Routes>
+		</Router>
 	);
 }
 

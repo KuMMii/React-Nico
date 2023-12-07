a3e3609ea6d678d273ef9125522826fafca61887
KuMMii <4.prgmng@gmail.com>
2023-12-07 22:52:39 +0900

    7-6 detail page

--- React/react-for-beginners/src/components/Movie.js
+++ React/react-for-beginners/src/components/Movie.js
@@ -1,12 +1,12 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-function Movie({ coverImg, title, year, summary, genres }) {
+function Movie({ id, coverImg, title, year, summary, genres }) {
 	return (
 		<div>
 			<img src={coverImg} alt={title} />
 			<h2>
-				<Link to="/movie">{title}</Link>
+				<Link to={`/movie/${id}`}>{title}</Link>
 			</h2>
 			<p>{year}</p>
 			<p>{summary}</p>
@@ -20,6 +20,7 @@ function Movie({ coverImg, title, year, summary, genres }) {
 }
 
 Movie.propTypes = {
+	id: PropTypes.number.isRequired,
 	coverImg: PropTypes.string.isRequired,
 	title: PropTypes.string.isRequired,
 	year: PropTypes.number.isRequired,
--- React/react-for-beginners/src/routes/Detail.js
+++ React/react-for-beginners/src/routes/Detail.js
@@ -1,4 +1,25 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
 function Detail() {
-	return <h1>Detail</h1>;
+	const { id } = useParams();
+	const [movie, setMovie] = useState([]);
+	const getMovie = async () => {
+		const json = await (
+			await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+		).json();
+		setMovie(json.data.movie);
+		// console.log(json.data.movie);
+	};
+	useEffect(() => {
+		getMovie();
+	}, []);
+	return (
+		<div>
+			<h1>{movie.title_long}</h1>
+			<img src={movie.medium_cover_image} alt="poster" />
+			<ul>{movie.genres ? movie.genres.map((g) => <li key={g}>{g}</li>) : <li>Loading</li>}</ul>
+		</div>
+	);
 }
 export default Detail;
